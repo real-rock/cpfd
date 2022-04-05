6c9fbdeca6a3d9e6fe61fcb40dcf26c3f7ac3bc9
Jinseok Heo <js.heo7747@gmail.com>
2022-04-05 20:58:29 +0900

    Feat: summary data added

---
+++ scripts/cpfd.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+BASE_IP=192.168.100.
+END_POINTS=(199 121 120 107 124 181 196)
+
+for e in "${END_POINTS[@]}"
+do
+	ip=${BASE_IP}${e}
+	echo "${ip}의 정보를 가져오고 있습니다..."
+
+	sshpass -p cpfd scp pi@${ip}:/home/pi/PMS7003/particle${e}.txt ./
+	echo "다운로드 성공"
+
+done
+
